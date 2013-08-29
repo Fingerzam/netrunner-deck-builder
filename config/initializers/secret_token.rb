0767f1d3a2077ae3ae78b9106b6e3cb498cd1ec3
Juhana Laurinharju <juhana.laurinharju@gmail.com>
2013-08-29 21:44:58 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netrunner::Application.config.secret_token = '5aac4d821f4e261d169c261e653c156873bff4323bd995f342f6e2d4fed8cea6f0b790d99e8df0d433dfa671d5f2446dbc964b82a2da2f710ef6d030a5c52390'
